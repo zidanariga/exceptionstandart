bc657f0d1ccd632e27d53c9c5b685b3fd8a37eeb
Apelicious <Apelicious@DESKTOP-7OGS3S6>
2023-06-21 15:49:05 +0700

    Add project files.

---
+++ exeptionstandart/exeptionstandart.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <exception>
+//untuk obyek exception yang akan di gunakan 
+#include <array>
+//untuk obyek array yang akan kit gunakan
+using namespace std;
+int main()
+{
+
+
+
+}
